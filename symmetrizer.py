67296f62f4b0fb1e63c129e3657e49719943c1ad
Jason Krone <jasonkrone@me.com>
2016-05-12 01:50:21 -0700

    added word aligned data

--- symmetrizer.py
+++ symmetrizer.py
@@ -6,23 +6,28 @@ from math import exp, pow, log
 
 class Symmetrizer(object):
 
+    # TODO: must parse files and put in e2f , f2e format
+    # these can be an array of sets of alignments
+    # change alignments to be 0 indexed and fit string indicies
     def __init__(e2f, f2e, fcorpus, ecorpus):
         self._e2f = e2f
         self._f2e = f2e
-        self._fcorpus = fcorpus
-        self._ecorpus = ecorpus
+        self._fcorp = fcorp
+        self._ecorp = ecorp
         # phrase_translations[englist_phrase][foreign_phrase] = log_prob
         self.phrase_translations = defaultdict(defaultdict(lambda : float("-inf")))
         # the distortion constant
         self.alpha = float()
 
-    ''' fills the phrase translation table using e2f and f2e '''
+
+    ''' fills the phrase translation table using e2f and f2e alignments'''
     def symmetrize(self):
         phrase_pairs = set()
-        zp = zip(self._fcorpus, self._ecorpus)
-        for (f_sent, e_sent) in zp:
-            alignment = self._grow_diag_final(e_sent, f_sent)
-            phrase_pairs.union(self._extract_phrase_pairs(alignment))
+        zp = zip(self._e2f, self._f2e, self._ecorp, self._fcorp)
+        # loop through sentence alignements and sentences
+        for (e2f_align, f2e_align, e_sent, f_sent) in zp:
+            alignment = self._grow_diag_final(e2f_align, f2e_align)
+            phrase_pairs.union(self._extract_phrase_pairs(alignment, e_sent, f_sent))
 
         # fill phrase_translations (probably using regex).
         # phi(t|s) = count(s, t) / count(s)
@@ -33,62 +38,51 @@ class Symmetrizer(object):
         # can use chi squared stats based pruning (prob not this)
 
 
-    ''' uses MLE to compute the best alignment for the given sentance '''
-    def _alignment_for_sent(self, src_sent, trg_sent, word_alignments):
-        # TODO: figure out how to do word alignment for sentanaces
-        # might just do all word alignments that match
-        # i.e. trg(word) is in the sentance and then mark that as a poss
-        # i think that is right
-
-    def _extract_phrase_pairs(self, alignment):
-
-
-    ################# FUNCTIONS FOR ALIGNMENT ####################
-
-
     ''' ues grow-diag-final to create a alignment between e2f and f2e '''
-    def _grow_diag_final(self, e_sent, f_sent):
-        e_align = self._alignment_for_sent(e_sent, f_sent, self._e2f)
-        f_align = self._alignment_for_sent(f_sent, e_sent, self._f2e)
-        # TODO: may need to index e and f
-        intersect = self._intersect(e_align, f_align)
-        union     = self._union(e_align, f_align)
-        self._grow_diag(intersect, union)
-        final(e_alignment)
-        final(f_alignment)
+    def _grow_diag_final(self, e2f_align, f2e_align):
+        intersect = e2f_align.intersection(f2e_align)
+        union     = e2f_align.union(f2e_align)
+        alignment = self._grow_diag(intersect, union)
+        alignment = final(e2f_align, alignment)
+        alignment = final(f2e_align, alignment)
+        return alignment
+
+
+    def _extract_phrase_pairs(self, alignment, esent, fsent):
+        # will need to use self._fcorp and self._ecorp to get the aligns
+
+
+    ############# HELPER FUNCTIONS FOR GROW-DIAG-FINAL ##############
+
 
     ''' grows the alignment by selectively adding elems from union '''
     def _grow_diag(self, alignment, union):
         points_added = True
         while points_added:
             points_added = False
-            # for each aligned pair (e_i, f_i)
-            for e, f in alignment.items():
-                # for each neighboring point (e-new, f-new)
-                    # if e-new is not aligned or f-new not aligned
-                    # and (e-new, f-new) in union
-                        # add (e-new, f-new) to alignment
+            for aligned_pair in alignment:
+                for (e_new, f_new) in self._neighbors(aligned_pair):
+                    # e-new not aligned or f-new not aligned
+                    aligns = [(x, y) for x, y in alignment if x == e_new or y == f_new]
+                    if len(aligns) == 0 and (e_new, f_new) in union:
+                        alignment.union((e_new, f_new))
                         points_added = True
         return alignment
+
+
+    ''' returns list of possible neighboring alignments '''
+    def _neighbors(self, aligned_pair):
+        neighboring = [(-1,0),(0,-1),(1,0),(0,1),(-1,-1),(-1,1),(1,-1),(1,1)]
+        x, y = aligned_pair
+        points = [(x + s, y + t) for s, t in neighboring]
+        return filter(points, lambda p : p[0] >= 0 and p[1] >= 0)
+
 
     ''' adds select points from a to alignment '''
     def _final(a, alignment):
         # words aligned in a
-        for e-new, f-new in a:
-            # if e-new not aligned in align or f-new not aligned in align
-                # add (e-new, f-new) to alignment
+        for (e_new, f_new) in a:
+            aligns = [(x, y) for x, y in alignment if x == e_new or y == f_new]
+            if len(aligns) == 0:
+                alignment.union((e_new, f_new))
         return alignment
-
-
-    ''' returns the intersection of the two alignments '''
-    def _intersect(self, e_align, f_align):
-        # could do dictionaries for e_align and f_align
-        # that map english -> foreign
-        # then convert those into items
-        # make a set from items
-        # then make a dictionary again from that set
-        # return the dictionary
-
-    def _union(self, e_align, f_align):
-        # copy data struct used above
-
